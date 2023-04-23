8da664e59f5984824825757ebeada54f2fc9d765
mkdemkov <mkdemkov@edu.hse.ru>
2023-04-23 03:00:18 +0300

    hide token

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import logging
+import os
 import aiogram
+from dotenv import load_dotenv
+from aiogram.dispatcher.filters import Text
 from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from aiogram.utils.callback_data import CallbackData
+
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
-# Set up your bot and dispatcher objects
-bot = Bot(token='6250730357:AAGMdL7DHHta7WELb792S3YbcWTuza40rAE')
+load_dotenv()
+
+# Set up bot and dispatcher objects
+bot = Bot(token=os.getenv('BOT_TOKEN'))
 dp = Dispatcher(bot)
 
-# Define your keyboard
-keyboard = ReplyKeyboardMarkup(
+# Defined welcome keyboard
+welcome_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(text="btn1"),
@@ -32,25 +36,25 @@ keyboard = ReplyKeyboardMarkup(
 # Define the function to send the message with the keyboard
 async def send_keyboard(message: types.Message):
     logging.info("текст отправлен")
-    await message.answer("Choose a button:", reply_markup=keyboard)
+    await message.answer("Choose a button:", reply_markup=welcome_keyboard)
 
 
 # Define the function to handle button clicks
-async def button_click_handler(callback_query: types.CallbackQuery):
-    logging.info("ХЕНДЛЕР СЛОВИЛ")
-    button_text = callback_query.data
-    logging.info(button_text)
-    await bot.answer_callback_query(callback_query.id, text=f"You clicked {button_text}")
+async def button_click_handler(message: types.Message):
+    button_text = message.text
+    logging.info(f'User {message.from_user.username} clicked button {button_text}')
+    await message.answer(f'You clicked {button_text}')
 
 
+# Define the function to handle commands /start and /help
 @dp.message_handler(commands=['start', 'help'])
 async def send_start_message(message: types.Message):
-    logging.info("start clicked")
+    logging.info(f'User {message.from_user.username} user command {message.text}')
     await send_keyboard(message)
 
 
-# Add handlers for your bot
-dp.register_callback_query_handler(button_click_handler)
+# Add handlers to process button clicks
+dp.register_message_handler(button_click_handler, Text(equals=['btn1', 'btn2', 'btn3', 'btn4', 'btn5']), state='*')
 
 # Start the bot
 if __name__ == '__main__':
